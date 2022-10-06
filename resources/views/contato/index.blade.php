044014f2dec2425f59f869aac7665e98f8fa64ee
Camila Cordeiro da Rocha <camilaa7x@gmail.com>
2022-10-06 16:46:28 -0300

    segundo commit

--- resources/views/contato/index.blade.php
+++ resources/views/contato/index.blade.php
@@ -2,9 +2,13 @@
 @section('title', 'Listagem de Contatos')
 @section('content')
     <h1>Listagem de Contatos</h1>
+    <br>
+    <a class="btn btn-success" href="{{url('contatos/create')}}">Criar</a>
+    <br><br>
     <ul>
         @foreach ($contatos as $contato)
             <li><a href="{{url('contatos/'.$contato->id)}}">{{$contato->nome}}</a></li>
+    @endforeach
     </ul>
-    @endsection
+
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('contatos', ContatosController::class);
+Route::resource('contatos',ContatosController::class);
